7e27e8da42a5395acf780f818c012c2d93cbe8dc
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2015-02-12 17:04:29 +0100

    Full speed, fuck norm

--- srcs/compute_answer.c
+++ srcs/compute_answer.c
@@ -6,7 +6,7 @@
 /*   By: tgauvrit <tgauvrit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/09 14:43:28 by tgauvrit          #+#    #+#             */
-/*   Updated: 2015/02/11 17:25:51 by tgauvrit         ###   ########.fr       */
+/*   Updated: 2015/02/12 17:01:03 by tgauvrit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ static void	putcounter(unsigned long n)
 	char	txtspc[22];
 	int		ntxt;
 
+	if (!(n % 5 == 0))
+		return ;
 	ntxt = 21;
 	txtspc[ntxt] = '\0';
 	while (n > 0)
@@ -52,6 +54,14 @@ static int	check_answer(int *a, int size, int steps, int *answer)
 	return (check_a(a, sizes[1], size));
 }
 
+static int	skip_rrr(int *ans, int i, int steps)
+{
+	ans[i]++;
+	while (++i < steps)
+		ans[i] = RRR + 1;
+	return (0);
+}
+
 static int	leanificate_answer(int *ans, int steps, int a_size)
 {
 	int		i;
@@ -61,43 +71,46 @@ static int	leanificate_answer(int *ans, int steps, int a_size)
 	b_size = 0;
 	while (i < steps - 1)
 	{
+		if (a_size < 2)
+		{
+			if (ans[i] == RRR)
+				return (skip_rrr(ans, i, steps));
+			if (   ans[i] == SA
+				|| ans[i] == SS
+				|| ans[i] == RA
+				|| ans[i] == RR
+				|| ans[i] == RRA)
+			{
+				ans[i]++;
+				continue ;
+			}
+		}
+		if (b_size < 2)
+		{
+			if (ans[i] == RRR)
+				return (skip_rrr(ans, i, steps));
+			if (   ans[i] == SA
+				|| ans[i] == SS
+				|| ans[i] == RB
+				|| ans[i] == RR
+				|| ans[i] == RRA)
+			{
+				ans[i]++;
+				continue ;
+			}
+		}
+		if (   (a_size < 1 && ans[i] == PA)
+			|| (b_size < 1 && ans[i] == PB))
+		{
+			ans[i]++;
+			continue ;
+		}
 		if (ans[i] == SS && ans[i + 1] <= SS)
 			ans[i + 1] = SS + 1;
-		if ((ans[i] == SA || ans[i] == SB) && (ans[i] == ans[i + 1]))
+		if (   ((ans[i] == SA || ans[i] == SB) && (ans[i] == ans[i + 1]))
+			|| (ans[i] == PA && ans[i + 1] == PB)
+			|| (ans[i] == PB && ans[i + 1] == PA))
 			ans[i + 1] += 1;
-		if (ans[i] == PA && ans[i + 1] == PB)
-			ans[i + 1] += 1;
-		if (ans[i] == PB && ans[i + 1] == PA)
-			ans[i + 1] += 1;
-		// (void)a_size;
-		if (ans[i] == PA)
-		{
-			if (a_size < 1)
-			{
-				ans[i]++;
-				ans[i + 1] = SA;
-				continue ;
-			}
-			else
-			{
-				a_size--;
-				b_size++;
-			}
-		}
-		if (ans[i] == PB)
-		{
-			if (b_size < 1)
-			{
-				ans[i]++;
-				ans[i + 1] = SA;
-				continue ;
-			}
-			else
-			{
-				b_size--;
-				a_size++;
-			}
-		}
 		if ((ans[i] >= RA && ans[i] <= RR) && ans[i + 1] >= RRA)
 		{
 			ans[i] += 1;
@@ -105,27 +118,30 @@ static int	leanificate_answer(int *ans, int steps, int a_size)
 		}
 		if (ans[i] >= RRA && (ans[i + 1] >= RA && ans[i + 1] <= RR))
 			ans[i + 1] = RRA;
-		i++;
-	}
-	if (ans[i] == PA)
-	{
-		if (a_size < 1)
-			ans[i]++;
-		else
+		if (ans[i] == PA)
 		{
 			a_size--;
 			b_size++;
 		}
-	}
-	if (ans[i] == PB)
-	{
-		if (b_size < 1)
-			ans[i]++;
-		else
+		else if (ans[i] == PB)
 		{
 			b_size--;
 			a_size++;
 		}
+		i++;
+	}
+	if (   (a_size < 1 && ans[i] == PA)
+		|| (b_size < 1 && ans[i] == PB))
+		ans[i]++;
+	if (ans[i] == PA)
+	{
+		a_size--;
+		b_size++;
+	}
+	else if (ans[i] == PB)
+	{
+		b_size--;
+		a_size++;
 	}
 	if (b_size > 0)
 		return (0);
